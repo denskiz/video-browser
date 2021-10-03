c01431c97606c3d3323848cdbc4202605a6ebdb2
Dennis <dennisent@protonmail.com>
2021-10-03 18:37:02 +1100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <SearchBar @termChange="onTermChange"></SearchBar>
+    <div class="row">
+      <VideoDetail :video="selectedVideo" />
+      <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
+const API_KEY = 'AIzaSyCl2MO9HCg4eLVSy9U-DZ10eR_PunIi_ww';
+export default {
+  name: 'App',
+  components: {
+    SearchBar,
+    VideoList,
+    VideoDetail,
+  },
+  data() {
+    return { videos: [], selectedVideo: null };
+  },
+  beforeMount() {
+    this.onTermChange('buildings');
+  },
+  methods: {
+    onVideoSelect(video) {
+      this.selectedVideo = video;
+    },
+    onTermChange(searchTerm) {
+      axios
+        .get('https://www.googleapis.com/youtube/v3/search', {
+          params: {
+            key: API_KEY,
+            type: 'video',
+            part: 'snippet',
+            q: searchTerm,
+          },
+        })
+        .then((response) => {
+          this.videos = response.data.items;
+          this.onVideoSelect(response.data.items[0]);
+        });
+    },
+  },
+};
+</script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2 class="text-center">Video Search</h2>
+    <br />
+    <input @input="onInput" placeholder="Search for..." class="form-control" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  methods: {
+    onInput: function(event) {
+      this.$emit('termChange', event.target.value);
+    },
+  },
+};
+</script>
+
+<style scoped>
+input {
+  width: 40%;
+  margin: 0 auto;
+}
+div {
+  text-align: center;
+  margin: 20px;
+}
+</style>
